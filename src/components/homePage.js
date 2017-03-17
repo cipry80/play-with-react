c75e81846d558789f6f964facbe6d9544472810a
Ciprian Vacaru <cvacaru@sdl.com>
2017-03-17 15:38:35 +0200

    update

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,14 +2,14 @@
 const React = require('react');
 
 const Home = React.createClass({
-        render: function() {
-            return (
-                <div className="jumbotron">
-                    <h1>Play with React</h1>
-                    <p>React, Router and Flux</p>
-                </div>
-            );
-        }
+    render: function() {
+         return (
+            <div className="jumbotron">
+                <h1>Play with React</h1>
+                <p>React, Router and Flux</p>
+            </div>
+        );
+    }
 });
 
 module.exports = Home;
